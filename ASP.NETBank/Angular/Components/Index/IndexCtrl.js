a61d708351e2a8ed81cce258c18f02a30a9ca43d
su-5 <alert@DESKTOP-5UVGHBN>
2018-12-24 14:37:23 +0300

    add Angular

---
+++ ASP.NETBank/Angular/Components/Index/IndexCtrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    // controller class definintion
+    var indexController = function ($scope, $rootScope) {
+       
+    };
+
+    // register your controller into a dependent module 
+    angular
+        .module("Web.Controllers")
+        .controller("indexController", ["$scope", "$rootScope", indexController]);
+
+})();
---
+++ ASP.NETBank/Angular/appConfig.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("Web")
+        .config(["$stateProvider", "$urlRouterProvider", "$locationProvider", function ($stateProvider, $urlRouterProvider) {
+
+            $stateProvider.state("Index", {
+                url: "/Index",
+                templateUrl: "Angular/Components/Index/Index.html",
+                controller: "indexController"
+            });
+
+            //$stateProvider.state("mainPage/AdminPanel", {
+            //    url: "/adminPanel",
+            //    templateUrl: "Angular/Views/AdminPanel.html",
+            //    controller: "adminController"
+            //});
+
+            $urlRouterProvider.otherwise("/Index");
+        }]);
+})();
